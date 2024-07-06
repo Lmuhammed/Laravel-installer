a9fbe1628738c40217ba933e87b468507338d9a4
Lmuhammed <140099085+Lmuhammed@users.noreply.github.com>
2024-07-06 16:26:07 +0100

    fix some typos

--- LaraInstall.py
+++ LaraInstall.py
@@ -1,30 +1,33 @@
 import os
+
 clear_command="clear"
 LaravelCrucialFiles=[
     "composer.json",
     "package.json",
     "artisan",
 ]
+
+commands=[
+            "composer install",
+            "npm install",
+            "cp ./.env.example ./.env",
+            "php artisan key:generate",
+            "php artisan migrate",
+]
+
 for file in LaravelCrucialFiles:
     #Check if files exist
     if not (os.path.exists(file)):
         print(f"{file} does not exist.")
         exit(22)
     else:
-        commands=[
-            "composer install",
-            "npm install",
-            "cp ./.env.example ./.env",
-            "php artisan key:generate",
-            "php artisan migrate",
-        ]
         for command in commands:
             #execute commands
             return_value = os.system(command)
             if return_value == 0:
                 continue
             else:
-                print(f"{Command} failed with return value:", return_value)
+                print(f"{command} failed with return value:", return_value)
                 break
         #Ask to start local development server               
         artisan_serve="php artisan serve"
