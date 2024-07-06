57a5a24921c41b5261aaf70b782fc71924493c4d
Lmuhammed <140099085+Lmuhammed@users.noreply.github.com>
2024-07-06 16:49:28 +0100

    check is os module installed

--- LaraInstall.py
+++ LaraInstall.py
@@ -1,5 +1,10 @@
-import os
+try:
+    import os
+except ImportError:
+    print("In order for LARAVEL to work properly this script requires : 'os' module")
+    raise SystemExit(1)
 
+#var 
 clear_command="clear"
 LaravelCrucialFiles=[
     "composer.json",
@@ -15,33 +20,34 @@ commands=[
             "php artisan migrate",
 ]
 
+artisan_serve="php artisan serve"
+
+#end 
+
 for file in LaravelCrucialFiles:
     #Check if files exist
     if not (os.path.exists(file)):
-        print(f"{file} does not exist.")
-        exit(22)
+        print(f"In order for LARAVEL to work properly,the project folder needs to have several files, including the following: {file}")
+        exit(2)
+
+for command in commands:
+#execute commands
+    return_value = os.system(command)
+    if return_value == 0:
+        continue
     else:
-        for command in commands:
-            #execute commands
-            return_value = os.system(command)
-            if return_value == 0:
-                continue
-            else:
-                print(f"{command} failed with return value:", return_value)
-                break
-        #Ask to start local development server               
-        artisan_serve="php artisan serve"
-        os.system(clear_command)
-        while True:
-                opt = input("Run the local development server (y/n): ")
-                opt=opt.lower()
-                if opt == "y":
-                    os.system(clear_command)
-                    os.system(artisan_serve)
-                    break
-                elif opt == "n":
-                    print("Bye ...")
-                    exit(0)
-                else:
-                    print("Invalid option:", opt)
-                    break
+        print(f"{command} failed with return value:", return_value)
+        break
+    #Ask to start local development server               
+    os.system(clear_command)
+    while True:
+        opt = input("Run the local development server (y/n): ")
+        opt=opt.lower()
+        if opt == "y":
+            os.system(artisan_serve)
+            break
+        elif opt == "n":
+            print("Bye ...")
+            exit(2)
+        else:
+            print(f"unknown command : {opt} ")
